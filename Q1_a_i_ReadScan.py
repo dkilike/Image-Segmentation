bc44515a934d95135ffb003e93432cc9445728ef
dkilike <47953156+dkilike@users.noreply.github.com>
2019-04-25 10:43:35 +1200

    Create Q1_a_i_ReadScan.py

---
+++ Q1_a_i_ReadScan.py
@@ -0,0 +1,6 @@
+'''Please write a program to read the scan and print out
+The maximum voxel intensity
+The mean voxel intensity
+The coordinates of the centre of the image volume, in the scanner coordinate system.
+'''
+
