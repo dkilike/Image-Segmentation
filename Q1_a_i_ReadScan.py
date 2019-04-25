0368d18afc918d42d8e5e457565283402d52b33e
dkilike <47953156+dkilike@users.noreply.github.com>
2019-04-26 00:14:12 +1200

    Update Q1_a_i_ReadScan.py

--- Q1_a_i_ReadScan.py
+++ Q1_a_i_ReadScan.py
@@ -10,8 +10,9 @@ import matplotlib.pyplot as plt
 import glob
 
 # load the DICOM files
+DICOM_dir_path = r'D:\One Drive Daily\OneDrive\CT scan Image Segmentation\Image-Segmentation\DICOM data'
 files = []
-for fname in glob.glob(r'D:\One Drive Daily\OneDrive\CT scan Image Segmentation\Image-Segmentation\DICOM data\*', recursive=False):
+for fname in glob.glob(DICOM_dir_path+'\*', recursive=False):
     print("loading: {}".format(fname))
     files.append(pydicom.read_file(fname))
 print("file count: {}".format(len(files)))
@@ -38,3 +39,22 @@ img3d = np.zeros(img_shape)
 for i, s in enumerate(slices):
     img3d[:, :, i] = s.pixel_array
 
+print()
+print('Question 1 (a)')
+print('i.   The maximum voxel intensity is {}'.format(img3d.max()))
+print('ii.  The mean voxel intensity is {}'.format(img3d.mean()))
+
+# centre of the image volume is at (256.5,256.5) pixel position between the 100th and 101st slices
+ImagePlanePosition_of_100th_slice = np.array(slices[99].ImagePositionPatient)
+RowChangeInX_of_100th_slice = np.array(slices[99].ImageOrientationPatient[0:3]) * slices[99].PixelSpacing[0] * 256.5
+ColumnChangeInY_of_100th_slice = np.array(slices[99].ImageOrientationPatient[3:6]) * slices[99].PixelSpacing[1] * 256.5
+coordinate_of_100th_slice = ImagePlanePosition_of_100th_slice + RowChangeInX_of_100th_slice + ColumnChangeInY_of_100th_slice
+
+ImagePlanePosition_of_101th_slice = np.array(slices[100].ImagePositionPatient)
+RowChangeInX_of_101th_slice = np.array(slices[100].ImageOrientationPatient[0:3]) * slices[100].PixelSpacing[0] * 256.5
+ColumnChangeInY_of_101th_slice = np.array(slices[100].ImageOrientationPatient[3:6]) * slices[100].PixelSpacing[1] * 256.5
+coordinate_of_101th_slice = ImagePlanePosition_of_101th_slice + RowChangeInX_of_101th_slice + ColumnChangeInY_of_101th_slice
+
+coordinate_of_ImageVolumeCentre = (coordinate_of_100th_slice+coordinate_of_101th_slice)/2
+
+print('iii. coordinates of the centre of the image volume is {} mm'.format(list(coordinate_of_ImageVolumeCentre)))
